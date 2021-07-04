ad1ab7c89708c85accea30bbcf0596825298cdbe
shimla-sinthia <67505798+shimla-sinthia@users.noreply.github.com>
2021-07-04 21:26:01 +0600

    Shimla

---
+++ 04-07-2021/coin_cng_minimum_number_of_coins_Itarative.cpp
@@ -0,0 +1,39 @@
+#define endl '\n'
+#define ll long long
+#define mod             1000000007
+#define rr              return 0
+#define ff              first
+#define in              insert
+#define ss              second
+
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+       ll dp[15][10005];
+       ll n = coins.size();
+        //cout << n << endl;
+        for(int i = 0; i <= n ; i++)
+            for(int j = 0; j <= amount; j++)
+                dp[i][j] = mod;
+        
+       for(int i=0;i<=n;i++) dp[i][0]=0;
+        
+       for(int i = 1; i <= n; i++)
+       {
+           for(int j = 1; j <= amount; j++)
+           {
+               if(coins[i - 1] > j)
+                   dp[i][j] = dp[i - 1][j];
+               else
+               {
+                   dp[i][j] = min(dp[i - 1][j], 1 + dp[i][j - coins[i - 1]]);
+               }
+           }
+       }
+        if(dp[n][amount] == mod)
+            return -1;
+        else
+            return dp[n][amount];
+    }
+};
